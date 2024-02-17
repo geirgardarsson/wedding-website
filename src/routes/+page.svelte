b325daadd414c2714d657ae97d142a2570686d91
Geir Gardarsson <geir93@gmail.com>
2024-02-17 23:26:37 +0000

    adding nes styles and tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full h-screen flex flex-col bg-rose-400">
+  <div class="mx-auto my-16 flex flex-col text-white">
+    <h1 class="mx-auto mb-4 text-2xl">Brúðkaup Geirs og Rögnu</h1>
+    <h2 class="mx-auto mb-4 text-xl">Laugardaginn 6. júlí 2024</h2>
+    <p class="mx-auto">Hlökkum til að sjá ykkur!!</p>
+  </div>
+  <section class="icon-list m-auto">
+    <i class="nes-bulbasaur"></i>
+    <i class="nes-charmander"></i>
+    <i class="nes-squirtle"></i>
+  </section>
+</div>
