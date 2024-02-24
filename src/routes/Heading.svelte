c806541037e5af93b78e5364087e80f6a3481a49
Geir Gardarsson <geir93@gmail.com>
2024-02-24 13:22:36 +0000

    icons

--- src/routes/Heading.svelte
+++ src/routes/Heading.svelte
@@ -2,8 +2,11 @@
   <h1 class="mx-auto mb-8 sm:mb-4 text-xl sm:text-2xl">
     Brúðkaup Geirs og Rögnu
   </h1>
-  <h2 class="mx-auto mb-8 sm:mb-4 text-l sm:text-xl">
-    Laugardaginn 6. júlí 2024
+  <h2 class="mx-auto mb-8 sm:mb-4 text-xs sm:text-xl flex">
+    <i
+      class="fa-regular fa-calendar m-auto text-base sm:text-lg align-middle mr-2"
+    ></i>
+    <p class="m-auto align-middle pt-1">Laugardaginn 6. júlí 2024</p>
   </h2>
   <p class="mx-auto">Hlökkum til að sjá ykkur!!</p>
 </div>
