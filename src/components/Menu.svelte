5549e296f8b83d49380feaa902285813200960cf
Geir Gardarsson <geir@travelshift.com>
2024-06-11 23:01:29 +0000

    menu

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import austurIndia from "$lib/austurindia.png";
   import Button from "./Button.svelte";
   import MenuItem from "./MenuItem.svelte";
@@ -13,6 +13,54 @@
 
     dessertSection.scrollIntoView({ behavior: "smooth" });
   }
+
+  let detailsVisible = false;
+  let courseName = "";
+  let courseDescription = "";
+
+  function setDetails(
+    visible: boolean,
+    course?: { name: string; description: string }
+  ) {
+    detailsVisible = visible;
+
+    if (!course) {
+      return;
+    }
+
+    courseName = course.name;
+    courseDescription =
+      course.description === ""
+        ? "Matseðillinn verður staðfestur innan skamms"
+        : course.description;
+  }
+
+  const courses = {
+    main1: {
+      name: "Tandori kjúklingur á teini",
+      description: "",
+    },
+    main2: {
+      name: "Lambaréttur",
+      description: "",
+    },
+    main3: {
+      name: "Chana Masala",
+      description: "",
+    },
+    veggie1: {
+      name: "Grænmetisréttur 1",
+      description: "",
+    },
+    veggie2: {
+      name: "Grænmetisréttur 2",
+      description: "",
+    },
+    veggie3: {
+      name: "Chana Masala",
+      description: "",
+    },
+  };
 </script>
 
 <Section
@@ -20,6 +68,20 @@
   backgroundColor="bg-orange-400"
   additionalClasses="flex-col h-screen justify-between"
 >
+  {#if detailsVisible}
+    <button
+      on:click={() => setDetails(false)}
+      class="fixed-center menu-details w-3/4 h-3/4 max-w-96 rounded text-amber-950 flex flex-col p-4 justify-around"
+    >
+      <h2 class="m-auto">{courseName}</h2>
+      <img
+        class="w-24 h-24 m-auto"
+        src={austurIndia}
+        alt="Austur-Indía Fjélagið"
+      />
+      <p class="m-auto">{courseDescription}</p>
+    </button>
+  {/if}
   <div class="flex flex-col mx-auto mt-4 md:w-2/3">
     <div class="nes-container is-rounded !mx-6 mt-4 !p-4 bg-white">
       <h1 class="w-full text-sm text-center m-auto">
@@ -34,9 +96,19 @@
       </div>
     </div>
     <div class="m-auto">
-      <MenuItem icon="fa-drumstick-bite" />
-      <MenuItem icon="fa-bowl-rice" />
-      <MenuItem icon="fa-bowl-food" color="text-emerald-600" />
+      <MenuItem
+        toggleFunction={() => setDetails(true, courses.main1)}
+        icon="fa-drumstick-bite"
+      />
+      <MenuItem
+        toggleFunction={() => setDetails(true, courses.main2)}
+        icon="fa-bowl-rice"
+      />
+      <MenuItem
+        toggleFunction={() => setDetails(true, courses.main3)}
+        icon="fa-bowl-food"
+        color="text-emerald-600"
+      />
     </div>
   </div>
   <div class="flex flex-col">
@@ -46,9 +118,21 @@
       </div>
     </div>
     <div class="m-auto">
-      <MenuItem icon="fa-bowl-rice" color="text-emerald-600" />
-      <MenuItem icon="fa-plate-wheat" color="text-emerald-600" />
-      <MenuItem icon="fa-bowl-food" color="text-emerald-600" />
+      <MenuItem
+        toggleFunction={() => setDetails(true, courses.veggie1)}
+        icon="fa-bowl-rice"
+        color="text-emerald-600"
+      />
+      <MenuItem
+        toggleFunction={() => setDetails(true, courses.veggie2)}
+        icon="fa-plate-wheat"
+        color="text-emerald-600"
+      />
+      <MenuItem
+        toggleFunction={() => setDetails(true, courses.veggie3)}
+        icon="fa-bowl-food"
+        color="text-emerald-600"
+      />
     </div>
   </div>
   <div class="m-4 mx-auto w-96">
